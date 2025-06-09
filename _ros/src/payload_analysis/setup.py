e7526730996f00c332fcde95ed8dde5edad3b0f1
Enrico Moro <enrimoro003@gmail.com>
2025-06-09 17:33:38 +0200

    Add ros package

---
+++ _ros/src/payload_analysis/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'payload_analysis'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='morolinux',
+    maintainer_email='enrimoro003@gmail.com',
+    description='This package provides tools for dynamic payload analysis in robotics with a focus on torque calculations and external force handling.',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'torque_analysis = payload_analysis.torque_analysis:main'
+        ],
+    },
+)
