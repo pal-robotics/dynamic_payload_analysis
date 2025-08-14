9650ee0d35e18fc1111e7d247904cb66c51dd544
Enrico Moro <enrimoro003@gmail.com>
2025-08-14 15:49:01 +0200

    added removed files

---
+++ dynamic_payload_analysis_core/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+package_name = 'dynamic_payload_analysis_core'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    zip_safe=True,
+    maintainer='Enrico Moro',
+    maintainer_email='enrimoro003@gmail.com',
+    description='This package implements core functionalities for dynamic payload analysis in robotics, focusing on torque calculations and external force handling.',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'urdf-parser-py>=0.0.4',
+        'math',
+        'typing',
+        'pathlib',
+        'tempfile',
+        'os',
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
